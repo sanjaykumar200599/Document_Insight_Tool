2965aa7c5938248b3bbda7a436101e8ba600972a
Sanjay Kumar <sanjaykumar200599@gmail.com>
2025-08-30 20:26:54 +0530

    updated backend

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,11 +1,25 @@
 from fastapi import FastAPI
 from app.routers import upload, insights
 from app.database import Base, engine
+from fastapi.middleware.cors import CORSMiddleware
 
 # Create DB tables
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="AI Document Insight Tool")
+
+# Register routers
+app.include_router(upload.router, prefix="/upload-resume", tags=["Upload"])
+app.include_router(insights.router, prefix="/insights", tags=["Insights"])
+
+# ✅ Enable CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],  # frontend URL
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Register routers
 app.include_router(upload.router, prefix="/upload-resume", tags=["Upload"])
